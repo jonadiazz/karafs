c1c5d99d9aeb5c17442ac6df42e3b32300998b06
jonadiazz <jonadiazz@hotmail.com>
2015-04-19 17:35:58 -0500

    Initial Commit

---
+++ karafs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  karafs
+//
+//  Created by Rurouni on 4/19/15.
+//  Copyright (c) 2015 Rurouni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
